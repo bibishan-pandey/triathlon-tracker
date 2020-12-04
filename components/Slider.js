fff01d12eb17e5b8d3d60f731d3d83cc5b9d7a09
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-12-04 13:20:44 +0545

    feat: new slider component added

---
+++ components/Slider.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+
+const Slider = () => {
+  return (
+    <View>
+      <Text>Slider Component</Text>
+    </View>
+  );
+};
+
+export default Slider;
