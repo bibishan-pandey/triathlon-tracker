50647d21ba51c08d60186f12c0cb053b8b5a0b57
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-12-04 13:22:26 +0545

    feat: slider and stepper added to AddEntry component
    AddEntry component changed to render all the metrics and metric type

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
+
+import Slider from "./Slider";
+import Stepper from "./Stepper";
+
 import {getMetricMetaInfo} from "../utils/helpers";
 
 
@@ -44,9 +48,26 @@ export default class AddEntry extends Component {
   };
 
   render() {
+    const metaInfo = getMetricMetaInfo();
     return (
       <View>
-        {getMetricMetaInfo('bike').getIcon()}
+        {Object.keys(metaInfo).map((key) => {
+          const {getIcon, type, ...rest} = metaInfo[key];
+          const value = this.state[key];
+          return (
+            <View key={key}>
+              {getIcon()}
+              {type === 'slider'
+                ? <Slider value={value}
+                          onChange={(value) => this.slide(key, value)}
+                          {...rest}/>
+                : <Stepper value={value}
+                           onIncrement={() => this.increment(key)}
+                           onDecrement={() => this.decrement(key)}
+                           {...rest}/>}
+            </View>
+          );
+        })}
       </View>
     );
   }
