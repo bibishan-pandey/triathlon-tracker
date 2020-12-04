e8f1654fa89aae3ea9283cc9af71811786ade01c
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-12-04 13:00:56 +0545

    feat: add entry component added
    AddEntry component added that gets metric data from helpers and each object returned from helper object is included in the state with a default value of 0 as starting point. Also, an increment and decrement methods are added inside the component to let the metric value be changed as per the steps for each metric. A slider method is added for sleep and eat metric

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-import { Ionicons } from '@expo/vector-icons';
+import AddEntry from "./components/AddEntry";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Ionicons name="ios-pizza" size={50} color="black" />
-    </View>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <AddEntry/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
