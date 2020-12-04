db1e1a02a118bf94a4f80e188969786da86cfa3c
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-12-04 13:20:58 +0545

    feat: new stepper component added

---
+++ components/Stepper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+
+const Stepper = () => {
+  return (
+    <View>
+      <Text>Stepper Component</Text>
+    </View>
+  );
+};
+
+export default Stepper;
