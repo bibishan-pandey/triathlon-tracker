812d49a343bfc2d734127f613ab1d2969bd7d8ef
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-12-07 13:00:33 +0545

    feat: added entries reducers in redux store

---
+++ store/reducers/index.js
@@ -0,0 +1,19 @@
+import {ADD_ENTRY, RECEIVE_ENTRIES} from "../actions/actionTypes";
+
+
+export default function entries(state = {}, action) {
+    switch (action.type) {
+        case RECEIVE_ENTRIES:
+            return {
+                ...state,
+                ...action.entries,
+            };
+        case ADD_ENTRY:
+            return {
+                ...state,
+                ...action.entry,
+            };
+        default:
+            return state;
+    }
+}
