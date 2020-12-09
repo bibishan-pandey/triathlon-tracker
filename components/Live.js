7e81715b03c9a7df58d42d4280077675c4d5d3b2
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-12-09 12:19:30 +0545

    feat: added styles and view for undetermined status

--- components/Live.js
+++ components/Live.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
-import { ActivityIndicator, View, Text, StyleSheet } from "react-native";
+import {
+  ActivityIndicator,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { Foundation } from "@expo/vector-icons";
+import { purple, red, white } from "../utils/colors";
 
 class Live extends Component {
   state = {
     coords: null,
-    status: null,
+    status: "undetermined",
     direction: "",
   };
+
+  askPermission = () => {};
 
   render() {
     const { status, coords, direction } = this.state;
@@ -19,8 +29,14 @@ class Live extends Component {
       );
     if (status === "undetermined")
       return (
-        <View>
-          <Text>Undetermined</Text>
+        <View style={styles.center}>
+          <Foundation name={"alert"} size={50} color={red} />
+          <Text style={{ color: red }}>
+            Please enable location services for this section
+          </Text>
+          <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+            <Text style={styles.buttonText}>Enable</Text>
+          </TouchableOpacity>
         </View>
       );
     return (
@@ -34,4 +50,27 @@ class Live extends Component {
 
 export default Live;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    backgroundColor: purple,
+    padding: 10,
+    alignSelf: "center",
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20,
+  },
+});
